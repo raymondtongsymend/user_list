a73b4d620d2579c476762d79c54bb220a420b248
AzureAD\RaymondTong <raymond.tong@symend.com>
2021-05-21 16:16:32 -0600

    Add keys

--- src/userList2.js
+++ src/userList2.js
@@ -2,13 +2,13 @@ import React from "react";
 import { GetClientObjects } from "./util/userDataListUtil";
 
 const UserList2 = ({ userDataList }) => {
-  let clientObjectList = GetClientObjects(userDataList);
+  const clientObjectList = GetClientObjects(userDataList);
 
   return (
     <div>
-      <h1> User List 2 </h1>
+      <h1> User List 2 - Not Composition </h1>
       {clientObjectList.map(({ name, vendors }) => (
-        <UserList2Client name={name} vendors={vendors} />
+        <UserList2Client name={name} vendors={vendors} key={name} />
       ))}
     </div>
   );
@@ -18,7 +18,7 @@ const UserList2Client = ({ name: clientName, vendors }) => (
   <div>
     <h2> Client: {clientName} </h2>
     {vendors.map(({ name, users }) => (
-      <UserList2Vendor name={name} users={users} />
+      <UserList2Vendor name={name} users={users} key={name} />
     ))}
   </div>
 );
@@ -28,11 +28,11 @@ const UserList2Vendor = ({ name: vendorName, users }) => (
     <h3> Vendor: {vendorName} </h3>
     <h4>Users</h4>
     {users.map(({ name }) => (
-      <UserList2User name={name} />
+      <UserList2User name={name} key={name} />
     ))}
   </div>
 );
 
-const UserList2User = ({ name: userName }) => <p>{userName}</p>;
+const UserList2User = ({ name: userName }) => <p key={userName}>{userName}</p>;
 
 export default UserList2;
