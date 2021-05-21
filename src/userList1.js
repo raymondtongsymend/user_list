37765b6c55f905b63297e50d75ed632316ca6ff2
AzureAD\RaymondTong <raymond.tong@symend.com>
2021-05-21 15:03:29 -0600

    Refactor UserList2

--- src/userList1.js
+++ src/userList1.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const UserList1 = ({ userDataList }) => (
   <div>
+    <h1>User List 1</h1>
     <ul>
       {userDataList.map(
         ({ name: userName, client: clientName, vendor: vendorName }) => (
--- src/userList2.js
+++ src/userList2.js
@@ -1,31 +1,69 @@
 import React from "react";
-import UserList2ClientBreakdown from "./userList2ClientBreakdown";
 
-const UserList2 = (props) => {
-  const userDataList = props.userDataList;
-
-  // create a distinct list of clients
-  let clientList = [];
-  userDataList.map((userData) => {
-    clientList.push(userData.client);
-  });
-  clientList = clientList.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-  });
+const UserList2 = ({ userDataList }) => {
+  let clientObjectList = GetClientObjects(userDataList);
 
   return (
     <div>
-      <h1>User List 2 - Grouped By Client / Vendor </h1>
-      <ul>
-        {clientList.map((client) => (
-          <UserList2ClientBreakdown
-            client={client}
-            userDataList={userDataList}
-          />
-        ))}
-      </ul>
+      <h1> User List 2 </h1>
+      {clientObjectList.map(({ name, vendors }) => (
+        <UserList2Client name={name} vendors={vendors} />
+      ))}
     </div>
   );
 };
+
+const UserList2Client = ({ name: clientName, vendors }) => (
+  <div>
+    <h2> Client: {clientName} </h2>
+    {vendors.map(({ name, users }) => (
+      <UserList2Vendor name={name} users={users} />
+    ))}
+  </div>
+);
+
+const UserList2Vendor = ({ name: vendorName, users }) => (
+  <div>
+    <h3> Vendor: {vendorName} </h3>
+    <h4>Users</h4>
+    {users.map(({ name }) => (
+      <UserList2User name={name} />
+    ))}
+  </div>
+);
+
+const UserList2User = ({ name: userName }) => <p>{userName}</p>;
+
+function GetClientObjects(userDataList) {
+  let clientObjectList = [];
+
+  userDataList.forEach(
+    ({ name: userName, client: userClient, vendor: userVendor }) => {
+      // find or create client object
+      let clientObject = clientObjectList.find(
+        ({ name: clientName }) => clientName === userClient
+      );
+      if (clientObject == null) {
+        clientObject = { name: userClient, vendors: [] };
+        clientObjectList.push(clientObject);
+      }
+
+      // find or create vendor object
+      let vendorObject = clientObject.vendors.find(
+        ({ name: vendorName }) => vendorName === userVendor
+      );
+      if (vendorObject == null) {
+        vendorObject = { name: userVendor, users: [] };
+        clientObject.vendors.push(vendorObject);
+      }
+
+      // add user
+      let userObject = { name: userName };
+      vendorObject.users.push(userObject);
+    }
+  );
+
+  return clientObjectList;
+}
 
 export default UserList2;
